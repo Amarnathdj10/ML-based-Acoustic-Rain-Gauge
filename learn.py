aa6cb9ba5a2afdb51e288a819f4621791b492ea1
Amarnathdj10 <amarnathdj56@gmail.com>
2026-06-10 11:36:20 +0530

    Initial commit

---
+++ learn.py
@@ -0,0 +1,23 @@
+import librosa
+import numpy as np
+
+audio, sr = librosa.load(r"D:\Coding journey\ML-based Acoustic Rain Gauge\audio files\hi.wav",sr=None)
+#print(audio)
+
+duration = len(audio)/sr
+#print(duration)
+
+stft = librosa.stft(audio)
+#print(stft)
+
+magnitude = np.abs(stft)
+#print(magnitude)
+#print(magnitude.shape)
+
+spectrogram = librosa.amplitude_to_db(magnitude)
+#print(spectrogram)
+#print(spectrogram.shape)
+
+mel_spec = librosa.feature.melspectrogram(y=audio,sr=sr)
+#print(mel_spec)
+#print(mel_spec.shape)
